a0c4f94e1f359fa70f36fe294c0e414ca3022de8
Max Meisinger <mmeising@student.42heilbronn.de>
2022-06-29 19:20:44 +0200

    using string size instead of search for NULL pointer now

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 11:52:23 by mmeising          #+#    #+#             */
-/*   Updated: 2022/06/28 13:01:26 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/06/29 19:20:05 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 std::string	str_toupper(std::string input)
 {
-	for (size_t i = 0; input[i]; i++)
+	size_t	size;
+
+	size = input.size();
+	for (size_t i = 0; i < size; i++)
 		input[i] = std::toupper(input[i]);
 	return (input);
 }
@@ -26,7 +29,6 @@ int	main(int argc, char **argv)
 
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	string = "";
 	for (size_t i = 1; argv[i]; i++)
 		string += str_toupper(argv[i]);
 	std::cout << string << std::endl;
