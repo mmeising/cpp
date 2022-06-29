89001c2c61b13567bd9b262f0ff4a96b45101cec
Max Meisinger <mmeising@student.42heilbronn.de>
2022-06-30 00:18:58 +0200

    trying different code style + no string buffer anymore

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,31 +6,27 @@
 /*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 11:52:23 by mmeising          #+#    #+#             */
-/*   Updated: 2022/06/29 19:20:05 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/06/29 23:56:51 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-std::string	str_toupper(std::string input)
-{
-	size_t	size;
-
-	size = input.size();
-	for (size_t i = 0; i < size; i++)
-		input[i] = std::toupper(input[i]);
-	return (input);
+std::string str_toupper(std::string input) {
+    for (size_t i = 0; i < input.size(); i++) {
+        input[i] = std::toupper(input[i]);
+    }
+    return (input);
 }
 
-int	main(int argc, char **argv)
-{
-	std::string	string;
-
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (size_t i = 1; argv[i]; i++)
-		string += str_toupper(argv[i]);
-	std::cout << string << std::endl;
-	return (0);
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    for (size_t i = 1; argv[i]; i++) {
+        std::cout << str_toupper(argv[i]);
+    }
+    std::cout << "\n";
+    return (0);
 }
