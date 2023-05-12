59d7182f1567345e2e2ca4d6cf302c74b0533437
Max Meisinger <maxilos771+42git@gmail.com>
2023-05-12 22:28:25 +0200

    merge sort hooray

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 19:07:25 by mmeising          #+#    #+#             */
-/*   Updated: 2023/05/12 22:19:48 by mmeising         ###   ########.fr       */
+/*   Updated: 2023/05/12 22:28:10 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,6 @@ PmergeMe::vecVec PmergeMe::splitVector(std::vector<int> vec) {
     }
     all.push_back(vec);
     vec.clear();
-    // printVectors(all);
     return (all);
 }
 
@@ -102,7 +101,6 @@ void PmergeMe::insertionOneVector(std::vector<int>& vec) {
     PmergeMe::vecIter it;
     PmergeMe::vecIter ite = vec.begin();
 
-    // std::cout << "insertionOneVector called" << std::endl;
     ite++;
     while (ite != vec.end()) {//cycling through entries of one vector
         it = vec.begin();
@@ -130,68 +128,48 @@ PmergeMe::vecVec PmergeMe::insertionVector(PmergeMe::vecVec& all) {
 }
 
 std::vector<int> PmergeMe::mergeTwoVecs(std::vector<int> vec1, std::vector<int> vec2) {
-    std::cout << "called mergeTwoVecs\n" << std::endl;
     PmergeMe::vecIter   it1;
     PmergeMe::vecIter   it2;
     std::vector<int>    ret;
 
-    std::cout << "want to merge: ";
-    printVector(vec1);
-    std::cout << "\nand: ";
-    printVector(vec2);
-    std::cout << std::endl;
 
     ret.reserve(vec1.size() + vec2.size());
-    std::cout << "mergeTwoVecs first loop" << std::endl;
-    while (vec1.size() && vec2.size()) {
-        it1 = vec1.begin();
-        it2 = vec2.begin();
+    it1 = vec1.begin();
+    it2 = vec2.begin();
+    while (it1 != vec1.end() && it2 != vec2.end()) {
         if (*it1 < *it2) {
             ret.push_back(*it1);
-            vec1.erase(it1);
+            it1++;
         } else {
             ret.push_back(*it2);
-            vec1.erase(it2);
+            it2++;
         }
     }
-    std::cout << "mergeTwoVecs second loop" << std::endl;
-    while (vec1.size() > 0) {
-        it1 = vec1.begin();
+    while (it1 != vec1.end()) {
         ret.push_back(*it1);
-        vec1.erase(it1);
+        it1++;
     }
-    std::cout << "mergeTwoVecs third loop" << std::endl;
-    while (vec2.size() > 0) {
-        std::cout << "vec.size is " << vec2.size() << std::endl;
-        it2 = vec2.begin();
+    while (it2 != vec2.end()) {
         ret.push_back(*it2);
-        vec2.erase(it2);
-        std::cout << "erased in last loop" << std::endl;
+        it2++;
     }
-    std::cout << "done with mergeTwo" << std::endl;
     return (ret);
 }
 
 std::vector<int> PmergeMe::mergeVector(PmergeMe::vecVec& all) {
     PmergeMe::vecVec all_2;
 
-    std::cout << "called mergeVector" << std::endl;
     while (all.size() > 1) {
-        std::cout << "first while intern, all.size() is " << all.size() << std::endl;
         all_2 = all;
         all.clear();
         PmergeMe::vecVecIter ite = all_2.begin();
         while (ite != all_2.end() && ++ite != all_2.end()) {
-            std::cout << "in the second while" << std::endl;
             ite = all_2.begin();
             all.push_back(mergeTwoVecs(*ite, *(++ite)));
-            std::cout << "ABOUT TO ERASE" << std::endl;
             all_2.erase(all_2.begin());
-            std::cout << "ABOUT TO ERASE 2" << std::endl;
             all_2.erase(all_2.begin());
             ite = all_2.begin();
         }
-        std::cout << "after while in mergeVector" << std::endl;
         if (all_2.size() > 0)
             all.push_back(*(all_2.begin()));
         all_2.clear();
@@ -210,7 +188,7 @@ void PmergeMe::sortVector(char** argv) {
     all = splitVector(vec);
     insertionVector(all);
     vec = mergeVector(all);
-    // printVector(vec);
+    printVector(vec);
     t = clock() - t;
     printf("sortVector took %lu clicks, %f seconds (%d clocks per second)\n", t, ((float)t) / CLOCKS_PER_SEC, CLOCKS_PER_SEC);
 }
